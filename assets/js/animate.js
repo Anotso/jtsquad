0b3c3a8dc63708053459d05a5cec1f69ea9f9caf
Anotso <40184359+Anotso@users.noreply.github.com>
2020-07-20 00:03:38 -0300

    Envio de todos os arquivos. Função de digitação já está funcionando

---
+++ assets/js/animate.js
@@ -0,0 +1,8 @@
+var typed4 = new Typed('#alternation', {
+    strings: ['E-commerce', 'Site', 'Sistema web', 'Blog', 'SEO'],
+    typeSpeed: 30,
+    backSpeed: 30,
+    backDelay: 1000,
+    startDelay: 2000,
+    loop: true
+});
